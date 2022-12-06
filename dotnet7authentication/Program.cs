b01fdcaa952e5d3270fcfdc864a23f96fa2ace8d
Isaac Oselukwue <princeizak@live.com>
2022-12-06 19:20:04 +0100

    Add project files.

---
+++ dotnet7authentication/Program.cs
@@ -0,0 +1,100 @@
+using Microsoft.AspNetCore.DataProtection;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDataProtection();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<AuthService>();
+
+var app = builder.Build();
+
+app.Use((ctx, next) =>
+{
+    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
+    var protector = idp.CreateProtector("auth-cookie");
+    var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
+    string protectedPayload = authCookie.Split("=").Last();
+    var payload = protector.Unprotect(protectedPayload);
+    var parts = payload.Split(":");
+    var key = parts[0];
+    var value = parts[1];
+
+    var claims = new List<Claim>();
+    claims.Add(new Claim(key, value));
+    var identity = new ClaimsIdentity(claims);
+    ctx.User = new System.Security.Claims.ClaimsPrincipal();
+
+
+    return next();
+});
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/username", (HttpContext ctx) =>
+{
+    
+    return ctx.User.FindFirst("usr").Value;
+}).WithOpenApi();
+
+app.MapGet("/login", async (AuthService auth) =>
+{
+    await auth.SignIn();
+    return "ok";
+}).WithOpenApi();
+
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+public class AuthService
+{
+    private readonly IDataProtectionProvider _idp;
+    private readonly IHttpContextAccessor _accessor;
+    public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
+    {
+        _idp = idp;
+        _accessor = accessor;
+    }
+    public async Task SignIn()
+    {
+        var protector = _idp.CreateProtector("auth-cookie");
+        _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:isaac")}";
+    }
+}
